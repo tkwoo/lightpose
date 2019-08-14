9e3bdc90d6122f9b5ca9c297389553a875b6b372
tkwoo <wtk1101@gmail.com>
2019-08-14 11:47:57 +0900

    [update] human count

--- onnx_inference.py
+++ onnx_inference.py
@@ -9,8 +9,8 @@ import torch
 from pycocotools.coco import COCO
 
 from human_detector import HumanDetector
-from human_keypoint import HumanKeypoint
-# from onnx_keypoint import HumanKeypoint
+# from human_keypoint import HumanKeypoint
+from onnx_keypoint import HumanKeypoint
 
 def get_human(detector, img_queue, bbox_queue):
     ''' This func is used for multiprocess '''
@@ -22,7 +22,7 @@ def get_human(detector, img_queue, bbox_queue):
 def main():
     coco = COCO('./utils/tkwoo_annotation.json')
 
-    vc = cv2.VideoCapture('./test_video/pose_test.mp4')
+    vc = cv2.VideoCapture('./human_cnt1.mp4')
 
     total_start = cv2.getTickCount()
 
@@ -40,6 +40,7 @@ def main():
     print ('[INFO] model loading time: %.3fms'%time)
 
     cnt = 0
+    aver_human = 0
     while True:
         cnt += 1
         if cnt > 1000:
@@ -55,14 +56,25 @@ def main():
         box_time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
         
         start = cv2.getTickCount()
-        annotations = keypoint.get_humankeypoints(img_orig, objs)
-
+        if len(objs) > 0:
+            annotations = keypoint.get_humankeypoints(img_orig, objs)
         time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
-        print ('[INFO:%d] processing time: %.3fms, %.3fms'%(cnt, box_time, time))
+        
+        cnt_human = 0
+        for ann in annotations:
+            if np.count_nonzero(ann['keypoints'][2::3]) > 4:
+                cnt_human += 1
+        aver_human = (cnt-1)*aver_human/cnt + cnt_human/cnt
+
+        print ('[INFO:%d] times: (%.1fms,%.1fms), Human:%d, Aver:%.1f'%(cnt, box_time, time, cnt_human, aver_human))
         
         ### coco style prediction & draw the result
         cats = coco.loadCats(coco.getCatIds())[0]
         show = usrcoco.drawAnns(img_orig, annotations, cats)
+
+        text = 'Human:%d, Aver:%.1f'%(cnt_human, aver_human)
+        cv2.putText(show,text,(10,20),1,cv2.FONT_HERSHEY_PLAIN, (128,255,255), 1, cv2.LINE_8)
+        cv2.putText(show,'%.1ffps'%(1000/(time+box_time)),(w-70,20),1,cv2.FONT_HERSHEY_PLAIN, (0,30,0), 1, cv2.LINE_8)
 
         cv2.imshow('show', show)
         key = cv2.waitKey(1)
--- torch_inference.py
+++ torch_inference.py
@@ -3,12 +3,13 @@ import cv2
 import numpy as np
 from utils import usrcoco
 import torch
+from os.path import join
 
 from pycocotools.coco import COCO
 
 from human_detector import HumanDetector
-# from human_keypoint import HumanKeypoint
-from onnx_keypoint import HumanKeypoint
+from human_keypoint import HumanKeypoint
+# from onnx_keypoint import HumanKeypoint
 
 
 def main():
@@ -20,9 +21,15 @@ def main():
     time = (cv2.getTickCount() - total_start) / cv2.getTickFrequency() * 1000
     print ('[INFO] model loading time: %.3fms'%time)
 
-    vc = cv2.VideoCapture('./test_video/pose_test.mp4')
+    vc = cv2.VideoCapture('./human_cnt1.mp4')
+
+    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    # ret, img_orig = vc.read()
+    # h,w = img_orig.shape[:2]
+    # out = cv2.VideoWriter('HPE.mp4',fourcc, 30.0, (w,h))
 
     cnt = 0
+    aver_human = 0
     while True:
         cnt += 1
         if cnt > 1000:
@@ -38,18 +45,33 @@ def main():
         # print ('[INFO] box time:%.3fms'%(time))
 
         start = cv2.getTickCount()
-        annotations = keypoint.get_humankeypoints(img_orig, objs)
+        if len(objs) > 0:
+            annotations = keypoint.get_humankeypoints(img_orig, objs)
         time = (cv2.getTickCount() - start) / cv2.getTickFrequency() * 1000
-        print ('[INFO:%d] processing time: %.3fms, %.3fms'%(cnt, box_time, time))
-        
+
+        cnt_human = 0
+        for ann in annotations:
+            if np.count_nonzero(ann['keypoints'][2::3]) > 4:
+                cnt_human += 1
+        aver_human = (cnt-1)*aver_human/cnt + cnt_human/cnt
+
+        print ('[INFO:%d] times: (%.1fms,%.1fms), Human:%d, Aver:%.1f'%(cnt, box_time, time, cnt_human, aver_human))
+
         ### coco style prediction & draw the result
         cats = coco.loadCats(coco.getCatIds())[0]
         show = usrcoco.drawAnns(img_orig.copy(), annotations, cats)
+
+        text = 'Human:%d, Aver:%.1f'%(cnt_human, aver_human)
+        cv2.putText(show,text,(10,20),1,cv2.FONT_HERSHEY_PLAIN, (128,255,255), 1, cv2.LINE_8)
+        cv2.putText(show,'%.1ffps'%(1000/(time+box_time)),(w-70,20),1,cv2.FONT_HERSHEY_PLAIN, (0,30,0), 1, cv2.LINE_8)
+        
+        # out.write(show)
 
         cv2.imshow('show', show)
         key = cv2.waitKey(1)
         if key == 27:
             break
+        
     
     total_time = (cv2.getTickCount() - total_start) / cv2.getTickFrequency()
     print ('[INFO] total time: %.3fs'%total_time)
